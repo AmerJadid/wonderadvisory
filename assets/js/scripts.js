2b44de4fe26355366184682b8dbc1b8a1b369acc
Amer Jadid <amer.jadid@cryppro.com>
2024-01-21 18:24:08 +0200

    Ref: add animation to header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,12 @@ menuTrigger.onclick = function() {
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+window.addEventListener('scroll', function() {
+    var logo = document.querySelector('.logo');
+    if (window.scrollY > 0) {
+        logo.classList.add('no-margin');
+    } else {
+        logo.classList.remove('no-margin');
+    }
+})
